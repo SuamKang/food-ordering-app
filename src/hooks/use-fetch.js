aa4f6eddd7d06409ce30de3fa062aa984eea6705
Suam Kang <ksa199653@gmail.com>
2023-08-07 15:45:37 +0900

    feat: fetch custom hook 추가

---
+++ src/hooks/use-fetch.js
@@ -0,0 +1,38 @@
+import { useState, useCallback } from "react";
+
+const useFetch = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error("REQUEST FAILED!");
+      }
+
+      const data = await response.json();
+
+      applyData(data);
+    } catch (error) {
+      setError(error.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useFetch;
