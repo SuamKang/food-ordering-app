d3a08f216bd74c617302821c69f01f499e42c582
Suam Kang <ksa199653@gmail.com>
2023-08-07 15:46:36 +0900

    feat: custom훅 적용 후 더미데이터 삭제

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -4,77 +4,36 @@ import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
 import classes from "./AvailableMeals.module.css";
-
-// const DUMMY_MEALS = [
-//   {
-//     id: "m1",
-//     name: "Sushi",
-//     description: "Finest fish and veggies",
-//     price: 22.99,
-//   },
-//   {
-//     id: "m2",
-//     name: "Schnitzel",
-//     description: "A german specialty!",
-//     price: 16.5,
-//   },
-//   {
-//     id: "m3",
-//     name: "Barbecue Burger",
-//     description: "American, raw, meaty",
-//     price: 12.99,
-//   },
-//   {
-//     id: "m4",
-//     name: "Green Bowl",
-//     description: "Healthy...and green...",
-//     price: 18.99,
-//   },
-// ];
+import useFetch from "../../hooks/use-fetch";
 
 function AvailableMeals(props) {
   const [meals, setMeals] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+
+  const { isLoading, error, sendRequest: getFetching } = useFetch();
 
   useEffect(() => {
-    // http get
-    const fetchGetRequest = async () => {
-      setIsLoading(true);
-      setError(null);
-
-      const response = await fetch(
-        "https://food-ordering-app-2401d-default-rtdb.firebaseio.com/meals.json"
-      );
-
-      if (!response.ok) {
-        throw new Error("Something went wrong!");
-      }
-
-      const data = await response.json();
-      console.log(data); // {m1:{...}, m2:{...}, m3:{...}, m4:{...}}
-
+    const transformData = (responseData) => {
       let loadedMeals = [];
 
-      for (let key in data) {
+      for (let key in responseData) {
         loadedMeals.push({
           id: key,
-          name: data[key].name,
-          description: data[key].description,
-          price: data[key].price,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
         });
       }
 
       setMeals(loadedMeals);
-      setIsLoading(false);
     };
-    // fetchGetRequest는 비동기 함수 -> 항상 promise 리턴 -> 오류를 가져오게 된다면 반환되는 promise객체가 거부된다. 따라서 오류에 대해서 try/catch처럼 외부에서 래핑하여 다룰 수 없고, promise내부 반환되는 메소드(.catch())를 사용해서 오류를 잡아주는것이 좋을것 같다.
 
-    fetchGetRequest().catch((error) => {
-      setIsLoading(false);
-      setError(error.message);
-    });
-  }, []);
+    getFetching(
+      {
+        url: "https://food-ordering-app-2401d-default-rtdb.firebaseio.com/meals.json",
+      },
+      transformData
+    );
+  }, [getFetching]);
 
   // elary return
   if (isLoading) {
